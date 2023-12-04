79598fe7a99b5927c780d12a8c3a5706e0d71b15
Michael Ewnetu <143202037+Ewnetu-Michael-27@users.noreply.github.com>
2023-12-03 22:38:25 -0500

    Update Heart_predict.py

--- Heart_predict.py
+++ Heart_predict.py
@@ -13,17 +13,17 @@ st.sidebar.success("Select a Page Above")
 df=pd.read_csv("df_heart_clean_2.csv")
 df_new=pd.read_csv("df_heart_clean_2.csv")
 
-button=st.button("Try!")
+
 
 list_val=st.multiselect("Pick varaibles",
                               ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", 
                                "thalach", "exang", "oldpeak", "slope", "ca", "thal", "num"] ,
                                ["age","sex","cp","thalach","exang","ca","thal"])
 
+button=st.button("Try!")
 if button:
     df_new=df[list_val]
     
 button_2=st.button("Export")
-
 if button_2:
     df_new.to_csv()
