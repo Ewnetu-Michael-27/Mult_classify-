2b4236843abad334dea4fa2856af424234a13efa
Michael Ewnetu <143202037+Ewnetu-Michael-27@users.noreply.github.com>
2023-12-03 22:37:45 -0500

    Update Heart_predict.py

--- Heart_predict.py
+++ Heart_predict.py
@@ -1,4 +1,6 @@
 import streamlit as st 
+import pandas as pd 
+
 
 st.set_page_config(
     page_title="Prediction of CAD"
@@ -8,4 +10,20 @@ st.title("Applying Several Classification Models to Predict the Presence of CAD"
 
 st.sidebar.success("Select a Page Above")
 
+df=pd.read_csv("df_heart_clean_2.csv")
+df_new=pd.read_csv("df_heart_clean_2.csv")
 
+button=st.button("Try!")
+
+list_val=st.multiselect("Pick varaibles",
+                              ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", 
+                               "thalach", "exang", "oldpeak", "slope", "ca", "thal", "num"] ,
+                               ["age","sex","cp","thalach","exang","ca","thal"])
+
+if button:
+    df_new=df[list_val]
+    
+button_2=st.button("Export")
+
+if button_2:
+    df_new.to_csv()
