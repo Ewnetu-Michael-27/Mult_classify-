53ba746d85eef8197035a1b43b60b11c8d6b98e1
Michael Ewnetu <143202037+Ewnetu-Michael-27@users.noreply.github.com>
2023-12-03 19:49:00 -0500

    Add files via upload

---
+++ Heart_predict.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title="Prediction of CAD"
+)
+
+st.title("Applying Several Classification Models to Predict the Presence of CAD")
+
+st.sidebar.success("Select a Page Above")
+
+
