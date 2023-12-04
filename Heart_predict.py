d240cc2b15c8deebe7f4847240cda19df4bcf1b4
Michael Ewnetu <143202037+Ewnetu-Michael-27@users.noreply.github.com>
2023-12-04 02:29:48 -0500

    Update Heart_predict.py

--- Heart_predict.py
+++ Heart_predict.py
@@ -16,6 +16,21 @@ st.title("Applying Several Classification Models to Predict the Presence of CAD"
 st.sidebar.success("Select a Page Above")
 
 
+b_1=st.button("Check_1")
+
+if b_1:
+    st.markdown("Continue")
+    b_2=st.button("Check_2")
+    if b_2:
+        st.markdown("Continue")
+        b_3=st.button("Check_3")
+        if b_3:
+            st.markdown("Done")
+            
+            
+        
+    
+
 
 
 
